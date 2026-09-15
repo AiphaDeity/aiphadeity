e7030f3896bd614e22deb366e12abbca427da69f
AiphaDeity <2835828489@qq.com>
2026-09-15 23:34:02 +0800

    feat(music): 更换播放器歌曲为 縁-えにし- 并添加封面

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -48,10 +48,10 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
-				cover: "/assets/music/cover/109951169585655912.webp",
+				name: "縁-えにし-",
+				artist: "S.E.N.S. (神思者)",
+				url: "/assets/music/SENS-Enishi.ogg",
+				cover: "/assets/music/cover/enishi-cover.webp",
 				lrc: "",
 			},
 		],
